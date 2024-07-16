aff17e863d44f7ca1c1f0ae7cf7c8ba796451b37
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-07-16 18:40:03 -0300

    Tela de cadastro (Programação)

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,3 +1,5 @@
+import { Cxmsg } from "./Cxmsg.js";
+
 // pegando o elemento do botão
 
 const btn_cadastro = document.getElementById("btn_enviar");
@@ -5,10 +7,37 @@ const btn_cadastro = document.getElementById("btn_enviar");
 btn_cadastro.addEventListener("click",()=>{
     // pegando os elementos do login
 
-    const input_email = document.getElementById("email").value;
-    const input_NomeUsu = document.getElementById("nomeUsu").value;
-    const input_Senha = document.getElementById("senhaUsu");
-    const input_confirmaSenha = document.getElementById("senhaUsuConfirma").value;
+    let input_email = document.getElementById("email");
+    let input_NomeUsu = document.getElementById("nomeUsu");
+    let input_Senha = document.getElementById("senhaUsu");
 
+    if(input_email.value.length == 0 || input_NomeUsu.value.length == 0 || input_Senha.value.length == 0){
+        const config = {
+            cor: "#800",
+            tipo: "ok"
+        }
+      return Cxmsg.mostrar(config,"TENTE NOVAMENTE", "Falta de dados para o cadastro!");
+    }
+
+    const endpoint = `http://127.0.0.1:1880/cadastroUsu/${input_email.value}/${input_NomeUsu.value}/${input_Senha.value}`;
+
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(res=>{
+        input_email.value = "";
+        input_NomeUsu.value = "";
+        input_Senha.value = "";
+        if(res.serverStatus == 2){
+            const config = {
+                cor: "blue",
+                tipo: "sn",
+                textos: ["SIM","NÃO"],
+                comando_sn: ()=>{
+                    window.open("../login.html","_self");
+                }
+            }
+             return Cxmsg.mostrar(config,"CADASTRO REALIZADO!", "Ir para a tela de login ?");
+        }
+    })
+        
 });
-
